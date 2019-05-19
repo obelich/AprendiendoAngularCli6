421c24e754e6deceaacd2a66c7dccc412c4e4bf5
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2019-05-19 12:59:42 -0600

    Se estan generando los componentes para mostrar los valores del api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { HttpClient } from 'selenium-webdriver/http';
 import { FormsModule } from '@angular/forms';
 
 import {HttpClientModule} from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent,
    ],
    imports: [
       BrowserModule,
